7930d17b6fe111442c236ebd7dbfe08ae58627f1
silviodantonio <silviodantonio.99@gmail.com>
2024-05-15 19:08:37 +0200

    Refactor some project constants

---
+++ main/project_constants.h
@@ -0,0 +1,17 @@
+#ifndef H_PROJECT_CONSTANTS
+#define H_PROJECT_CONSTANTS
+
+// Note: not all constants used in the project are defined here, some of them
+// have to be changed directly in the source file.
+
+#define ADC_SAMPLE_BUFFER_SIZE 2048
+// Probably would be better if this value is calculated at runtime
+#define WINDOWED_AVG_SAMPLES_NUM 5
+#define ADC_UNIT ADC_UNIT_1
+#define ADC_CHANNEL ADC_CHANNEL_6
+#define ADC_CLK_SRC ADC_DIGI_CLK_SRC_DEFAULT
+
+#define WIFI_SSID "iPhone di Silvio"
+#define WIFI_PASS ""
+
+#endif
